bb5c18090352e9df17788f8ed48c794ade40b6ca
jrodrigu256 <jrodrigu256@cuc.edu.co>
2024-09-24 12:42:41 -0500

    m1

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+import miLogo from "../src/assets/img/Kawasaki (1).png"
+
+function App() {
+  return (
+    <>
+      <div>
+        <img src={miLogo} alt="Logo Kawasaki" height={250} />
+        <h1>Bienvenoidos a mi pagina de Kawasaki</h1>
+        <p>Este es mi taller #3</p>
+      </div>
+    </>
+  )
+}
+
+export default App;
